9c5527c1e05b34fcbbeb8f9b75351254b3288daf
anthonexz1997 <72754190+anthonexz1997@users.noreply.github.com>
2020-10-26 01:07:00 +0800

    Add files via upload

---
+++ Multiplication Table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= 10; ++i) {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+    printf("\n\nProgrammed by: Mancia, Mark Anthony");
+    return 0;
+}
